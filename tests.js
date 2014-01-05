bb143cec8aa2946500d0258344d16de5db5412ca
Isao Yagi <isao.yagi@gmail.com>
2014-01-05 15:19:00 -0800

    tweak test to work with jshint 2.x

--- tests.js
+++ tests.js
@@ -10,23 +10,23 @@ test('returns false if lint is found', function(t) {
 });
 
 test('jshint errors are properties of itself', function(t) {
-    t.plan(2);
-    var expected = [{
+    t.plan(9);
+    var expected = {
             id: '(error)',
             raw: 'Missing semicolon.',
             evidence: 'var no_semicolon',
             line: 1,
             character: 17,
             scope: '(main)',
-            a: undefined,
-            b: undefined,
-            c: undefined,
-            d: undefined,
             reason: 'Missing semicolon.'
-        }];
+        };
 
     t.false(lint('var no_semicolon'));
-    t.same(lint.errors, expected);
+    t.same(lint.errors.length, 1);
+
+    Object.keys(expected).forEach(function (key) {
+    	t.same(lint.errors[0][key], expected[key]);
+    });
 });
 
 test('returns true if lint-free', function(t) {
